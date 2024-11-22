f5b711865fe5d13d13b5dd491169b82a2a716e3a
unknown <ajanks+entropypp@gmail.com>
2024-11-21 19:09:59 -0800

    add to git

---
+++ watch_address.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+ ./watch_address.py -f address.watch@ajanks.com -t ajanks@gmail.com -e smtp.ionos.com -u  'ajanks@gmail.com;6044172946@msg.telus.com' -p  Fg7LA6Uga7Kols6aJduk -x '192.168.1.206:4000' -a 'bc1qpuyalpvq087cjhqghg56m65z0mmfmzplpdsecp' -s 3129239
